a220755199e867c23997f2fe2da3e24829340f55
emiel hagen <emiel.hagen@gmail.com>
2020-10-15 10:46:42 +0200

    add type to core pallet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_085938) do
+ActiveRecord::Schema.define(version: 2020_10_15_084219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_10_13_085938) do
     t.integer "pallet_type_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "type"
     t.index ["label"], name: "index_pallets_on_label"
   end
 
